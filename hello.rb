8ff01eb8c5099ea308b1209303bd4c86b1588b0d
reefmuhammad <smuhamm9@uncc.edu>
2017-11-12 02:25:24 +0000

    Add an author/email comment

--- hello.rb
+++ hello.rb
@@ -5,4 +5,11 @@ puts "Hello, #{name}!"
 # Default is "World"
 name = ARGV.first || "World"
 
+
+# Default is World
+# Author: Jim Weirich (jim@somewhere.com)
+name = ARGV.first || "World"
+
 puts "Hello, #{name}!"
+
+
